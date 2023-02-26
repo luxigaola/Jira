60204f2ef709ae3493457b14a75b4a3bd086eb64
csh <caishaohua001@outlook.com>
2023-02-27 01:14:34 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+/*
+ * @Description:
+ * @Version: 2.0
+ * @Autor: user
+ * @Date: 2023-02-26 21:44:25
+ * @LastEditors: user
+ * @LastEditTime: 2023-02-27 00:22:33
+ */
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import utils from 'utils/utils';
+
+function App() {
+  console.log(utils);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.s
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
